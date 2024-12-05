9d1b5243a4e3bcd0925fe022eced94b5a38206c3
Bruno Sampaio <bpinhosilva@gmail.com>
2024-12-04 23:03:00 -0500

    Add day 3 part 2 code

--- 2024/day-03/day-03-part-2.py
+++ 2024/day-03/day-03-part-2.py
@@ -2,12 +2,21 @@ import sys
 import re
 
 sum = 0
+enabled = True
 
 # Read input until EOF
 for line in sys.stdin:
     line = line.strip()  # Strip leading/trailing spaces
 
     if line:
-        matches = re.findall("mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)", line)
+        for r in re.finditer("mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\)", line):
+            option, a, b = r.group(0, 1, 2)
+
+            if option == "don't()":
+                enabled = False
+            elif option == "do()":
+                enabled = True
+            elif enabled:
+                sum += int(a) * int(b)
 
 print(sum)
